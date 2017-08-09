455beabca0dbda78328818c19f6e648620e8faef
maligh <455080311@qq.com>
2017-08-10 00:53:52 +0800

    Init
    Init

---
+++ MLDesignPatterns-OC/MLDesignPatterns-OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MLDesignPatterns-OC
+//
+//  Created by mjpc on 2017/8/10.
+//  Copyright © 2017年 mali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
